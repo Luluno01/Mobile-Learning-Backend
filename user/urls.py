bd7ed0c621cf1ad925f822d6e6b25d12557b7ead
Untitled <gogs@fake.local>
2018-02-22 19:54:29 +0800

    change urls

--- user/urls.py
+++ user/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^login/$', views.login, name='login'),
     url(r'logout/$', views.logout, name='logout'),
     url(r'^reset/$', views.reset, name='reset'),
-    url(r'^favorite/((?P<type>\d+)/(?P<id>\d+))?$', views.Favorite.favorite, name='favorite'),
-    url(r'^flawbook/((?P<type>\d+)/(?P<id>\d+))?$', views.Flawbook.flawbook, name='flawbook')
+    url(r'^favorite/((?P<type>\d+)/(?P<id>\d+)/)?$', views.Favorite.favorite, name='favorite'),
+    url(r'^flawbook/((?P<type>\d+)/(?P<id>\d+)/)?$', views.Flawbook.flawbook, name='flawbook')
 ]
